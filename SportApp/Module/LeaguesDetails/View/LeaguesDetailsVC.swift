73b243d27526cd9f45366cc3c013d28abf3b4e57
EmyAbobakr <emyabobakr@EmyAbobakrs-Mac.local>
2022-02-06 14:11:26 -0800

    Added teams collection view

--- SportApp/Module/LeaguesDetails/View/LeaguesDetailsVC.swift
+++ SportApp/Module/LeaguesDetails/View/LeaguesDetailsVC.swift
@@ -20,12 +20,15 @@ class LeaguesDetailsVC: UITableViewController {
     var myPresenter = RouterDetails.presenter
     var resultView :[String]!
     
+    @IBOutlet weak var teamsCollectionView: UICollectionView!
+    
+    
 override func viewDidLoad() {
         super.viewDidLoad()
         animator()
         getData()
         setupTableView()
-        //setupCollectionView()
+        setupCollectionView()
         //myPresenter.printLeague()
     }
     
@@ -49,10 +52,11 @@ override func viewDidLoad() {
            self.tableView.delegate = self
            self.tableView.dataSource = self
        }
-//    func setupCollectionView() {
-//        self.teamsCollectionView.delegate = self
-//        self.teamsCollectionView.dataSource = self
-//    }
+    
+        func setupCollectionView() {
+            teamsCollectionView.delegate = self
+            teamsCollectionView.dataSource = self
+        }
 }
 
 extension LeaguesDetailsVC : LeaguesDetailsProtocol {
@@ -65,109 +69,34 @@ extension LeaguesDetailsVC : LeaguesDetailsProtocol {
             return item.strTeamBadge
         })
         //print(resultView ?? "no data")
-       self.tableView.reloadData()
+        self.tableView.reloadData()
+        self.teamsCollectionView.reloadData()
+        
         //self.teamsCollectionView.reloadData()
     }
 }
 
-extension LeaguesDetailsVC {
-    // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 1
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 3
-//    }
-
+extension LeaguesDetailsVC: UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return resultView?.count ?? 0
+        //return 10
+    }
     
-//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return CGFloat(292)
-//    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        if indexPath.row == 0{
-            let cell = tableView.dequeueReusableCell(withIdentifier: "upcomingTableCellID", for: indexPath) as! UITableViewCell
-            
-            return cell
-        }
-        else if indexPath.row == 1 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "latestTableCellID", for: indexPath) as! UITableViewCell
-            
-            return cell
-        }
-        let cell = tableView.dequeueReusableCell(withIdentifier: "teamsTableCellID", for: indexPath) as! TeamsTableViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "teamCellID", for: indexPath) as! TeamsCell
         
-        cell.teamsStrImages = resultView
-        cell.teamsCollectionView.tag = indexPath.row
+        let url = URL(string: resultView[indexPath.row])
+        cell.teamImage.kf.setImage(with: url)
+        
+        
         
         return cell
         
-        
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
 }
-//extension LeaguesDetailsVC :UICollectionViewDelegate, UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return resultView.count ?? 0
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "teamCellID", for: indexPath) as! TeamCell
-//        let imageURL = URL(string: resultView[indexPath.row])
-//        cell.teamImage.kf.setImage(with: imageURL)
-//        return cell
-//    }
-//
-//
-//}
 
 
---
+++ SportApp/Module/LeaguesDetails/View/TeamsCell.swift
@@ -0,0 +1,14 @@
+//
+//  TeamsCell.swift
+//  SportApp
+//
+//  Created by EmyAbobakr on 2/6/22.
+//  Copyright © 2022 EmyAbobakr. All rights reserved.
+//
+
+import UIKit
+
+class TeamsCell: UICollectionViewCell {
+    
+    @IBOutlet weak var teamImage: UIImageView!
+}
